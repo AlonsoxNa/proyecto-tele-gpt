68109b239e2105a0a22022d6f3cdbb0046297ce7
AlonsoxNa <aorostica20@alumnos.utalca.cl>
2024-04-24 13:00:56 -0400

    feat: se añaden botones en la vista principal para ir a las otras vistas

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+
 export const Login = () => {
   return (
-    <h1>Login</h1>
-  )
-}
+    <>
+      {/* Por ahora están estos botones, después obviamente hay que colocar el login */ }
+      <div className="d-flex justify-content-center align-items-center gap-4 vh-100">
+        <Link className="btn btn-primary" to="/crear-anuncio">Ir a crear anuncio</Link>
+        <Link className="btn btn-primary" to="/slider" >Ir a slider</Link>
+        <Link className="btn btn-primary" to="/dashboard" >Ir a dashboard</Link>
+      </div>
+    </>
+  );
+};
