08800582bd6f0cb37099a0cdea85b764cef2087c
Bastián Núñez <bastinuez@hotmail.com>
2024-05-14 23:41:55 -0400

    feat(frontend): avance componente slider

--- src/components/slider/Carrusel.tsx
+++ src/components/slider/Carrusel.tsx
@@ -1,40 +1,46 @@
-import Image1 from '@/assets/slider-01.avif';
-import Image2 from '@/assets/slider-02.jpg';
+import Image1 from '@/assets/publicacion2.jpg';
+import Image2 from '@/assets/publicacion3.jpg';
 import Image3 from '@/assets/slider-03.webp';
+import Video1 from '@/assets/publicacion1.mp4';
 import { CarruselItem } from './CarruselItem';
-
+import "./Carrusel.css"
 
 const itemSlider = [
   {
-    image: Image1,
-    label: 'Soy una descripción 1',
-    text: 'Soy un texto 1'
+    multimedia: Image1,
+    label: 'Titulo de la notica 1, prueba de cuanto deberia tener de largo1',
+    type: 'image',
+    text: 'Titulo de la notica 1, prueba de cuanto deberia tener de largo'
   },
   {
-    image: Image2,
-    label: 'Soy una descripción 2',
+    multimedia: Image2,
+    label: 'Titulo de la notica 2, prueba de cuanto deberia tener de largo2',
+    type: 'image',
     text: 'Soy un texto 2'
   },
   {
-    image: Image3,
-    label: 'Soy una descripción 3',
-    text: 'Soy un texto 3'
+    multimedia: Video1,
+    label: 'Titulo de la notica 3, prueba de cuanto deberia tener de largo3',
+    type: 'video',
+    text: 'Soy un texto video 1'
   }
+  //TAMBIEN DEBERIA RECIBIR LA DURACION DEL VIDEO PARA SETEAR ESTE
 ];
 
 export const Carrusel = () => {
   return (
-    <div className="container">
-      <div id="carouselExampleCaptions" className="carousel slide">
-        <div className="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
+    <div className="w-full">
+      <div id="carouselExampleCaptions" className="carousel slide carousel-container" data-bs-ride="carousel">
         <div className="carousel-inner">
-          <CarruselItem image={ itemSlider[ 0 ].image } label={ itemSlider[ 0 ].label } text={ itemSlider[ 0 ].text } />
-          <CarruselItem image={ itemSlider[ 1 ].image } label={ itemSlider[ 1 ].label } text={ itemSlider[ 1 ].text } />
-          <CarruselItem image={ itemSlider[ 2 ].image } label={ itemSlider[ 2 ].label } text={ itemSlider[ 2 ].text } />
+          <div className="carousel-item active" data-bs-interval="9000">
+            <CarruselItem type={itemSlider[0].type} image={ itemSlider[0].multimedia } label={ itemSlider[0].label } text={ itemSlider[0].text } />
+          </div>
+          <div className="carousel-item" data-bs-interval="9000">
+            <CarruselItem type={itemSlider[1].type} image={ itemSlider[1].multimedia} label={ itemSlider[1].label } text={ itemSlider[1].text } />
+          </div>
+          <div className="carousel-item" data-bs-interval="9000">
+            <CarruselItem type={itemSlider[2].type} image={ itemSlider[2].multimedia } label={ itemSlider[2].label } text={ itemSlider[2].text } />
+          </div>
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -45,8 +51,11 @@ export const Carrusel = () => {
           <span className="visually-hidden">Next</span>
         </button>
       </div>
-
-
+      <div className='w-full descripcion-container justify-content-center flex'>
+        <h1 className='text-center'>
+        Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original.
+        </h1>
+      </div>
     </div>
   );
 };
